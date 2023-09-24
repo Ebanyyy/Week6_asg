3e9e5b0a677944f8053f48335b556bb6cbe233c6
Ebanyyy <vaniegabu@gmail.com>
2023-09-24 22:19:44 +0800

    IMDb

--- data_management.rb
+++ data_management.rb
@@ -11,13 +11,25 @@ class DataManager
 			movie = Movie.create(
 				title: row[1],
 				release_year: row[2],
-				rating: row[6],
 				plot_summary: row[7],
 				director: row[9]
 				)
 		
 		end
 	end
+
+	def self.update_movies(filename, movies)
+
+		CSV.foreach(filename, headers: true) do |row|
+			movie = movies.fing_by(title: row[1])
+			movie.update(rating: row[6], poster: tow[0])
+		end
+	end
 end
 
+
+
 DataManager.load_movie('public/imdb_top_1000.csv')
+
+movies = Movie.all 
+DataManager.update_movies('public/imdb_top_1000', movies)
